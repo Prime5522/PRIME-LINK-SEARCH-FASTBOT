edc3ada60ede7450cc0745d400822bca4a70e8be
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-01-10 05:29:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import logging
 from flask import Flask
+
 app = Flask(__name__)
+
+# Set up logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%I:%M:%S %p'
+)
 
 @app.route('/')
 def hello_world():
+    app.logger.info("LazyURLhunter is running.")
     return 'LazyURLhunter is Running ! Smile Baby'
 
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

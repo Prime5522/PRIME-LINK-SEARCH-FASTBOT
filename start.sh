9b958ae7f157e353c17763be890a89566a1acf63
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-01-10 05:16:07 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
-cd /LazyUrlHunterrBOT
+git clone https://github.com/Prime5522/PRIME-LINK-SEARCH-FASTBOT.git /PRIME-LINK-SEARCH-FASTBOT
+cd /PRIME-LINK-SEARCH-FASTBOT
 pip3 install -U -r requirements.txt
-echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
+echo "⚡️⚡️ Starting PRIME-LINK-SEARCH-FASTBOT..."
 python3 main.py
